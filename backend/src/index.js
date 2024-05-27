57d3fdd214fe65fac17391d5dfb0a2d869f78b96
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-28 01:50:37 +0200

    fix imports in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 //index.js sirve para que arranque la aplicación
 
-import app from './app'
-import './database'
+import app from './app.js'
+import './database.js'
 
 app.listen(4000)
 console.log("Server listening on port",4000)
