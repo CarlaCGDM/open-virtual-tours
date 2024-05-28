af80f7dd0dffed5d409e5b8f5bf63a8c39fee93b
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-28 03:21:31 +0200

    testing build sync

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber'
 export default function Home() {
   return <>
     <div> Home page </div>
+    <div> Testing build sync </div>
     <Canvas>
         <mesh>
           <torusKnotGeometry />
