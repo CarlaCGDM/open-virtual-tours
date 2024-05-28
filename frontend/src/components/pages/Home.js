3db80613e78be7c16098b19c127ba91b0330d760
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-28 05:05:36 +0200

    test api call

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import { Canvas } from '@react-three/fiber'
+import TestAPICall from '../TestAPICall'
 
 export default function Home() {
   return <>
     <div> Home page </div>
     <div> Testing build sync again</div>
+    <TestAPICall />
     <Canvas>
         <mesh>
           <torusKnotGeometry />
