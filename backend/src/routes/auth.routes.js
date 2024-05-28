74e414572318a504347676079a21c9fba361c7d6
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-28 02:16:36 +0200

    fix more imports

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 const router = Router()
 
 import * as authController from '../controllers/auth.controller.js'
-import {verifySignup} from '../middlewares.js'
+import {verifySignup} from '../middlewares/verifySignup.js'
 
 router.post('/signup', [verifySignup.checkDuplicateUsernameOrEmail,verifySignup.checkRolesExist], authController.signUp)
 router.post('/signin', authController.signIn)
