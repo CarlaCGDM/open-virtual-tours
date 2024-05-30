179d45ba4af76d6edf76c00e7062d92832c52ca5
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-31 01:42:41 +0200

    cleanup

--- backend/src/routes/upload.routes.js
+++ backend/src/routes/upload.routes.js
@@ -20,25 +20,24 @@ const router = Router()
 // })
 
 
-
-router.post('/upload', upload.single('file'), async (req, res) => {
-    try {
-        const gfs = new Grid(mongoose.connection.db, mongoose.mongo);
-        const writeStream = gfs.createWriteStream({
-            filename: req.file.originalname,
-            mode: 'w',
-            content_type: req.file.mimetype,
-        });
-        fs.createReadStream(req.file.path).pipe(writeStream);
-        writeStream.on('close', (file) => {
-            fs.unlink(req.file.path, (err) => {
-                if (err) throw err;
-                return res.json({ file });
-            });
-        });
-    } catch (err) {
-        return res.status(400).json({ message: 'Error uploading file', error: err });
-    }
-});
+// router.post('/upload', upload.single('file'), async (req, res) => {
+//     try {
+//         const gfs = new Grid(mongoose.connection.db, mongoose.mongo);
+//         const writeStream = gfs.createWriteStream({
+//             filename: req.file.originalname,
+//             mode: 'w',
+//             content_type: req.file.mimetype,
+//         });
+//         fs.createReadStream(req.file.path).pipe(writeStream);
+//         writeStream.on('close', (file) => {
+//             fs.unlink(req.file.path, (err) => {
+//                 if (err) throw err;
+//                 return res.json({ file });
+//             });
+//         });
+//     } catch (err) {
+//         return res.status(400).json({ message: 'Error uploading file', error: err });
+//     }
+// });
 
 export default router
