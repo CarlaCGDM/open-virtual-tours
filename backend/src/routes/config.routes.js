e4c51324ed5d8d38c7006f661cf889eef4550604
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-30 20:04:02 +0200

    initial setup

--- backend/src/routes/config.routes.js
+++ backend/src/routes/config.routes.js
@@ -1,17 +1,11 @@
 import { Router } from 'express'
 const router = Router()
 
-import * as settingsController from '../controllers/globalsettings.controller.js'
+import * as configController from '../controllers/config.controller.js'
 import {authJwt} from '../middlewares/index.js'
-
-router.post('/', [authJwt.verifyToken, authJwt.isEditor], settingsController.createSetting)
 
 router.get('/', settingsController.getSettings)
 
-router.get('/:settingId', settingsController.getSettingById)
 router.put('/:settingId', [authJwt.verifyToken, authJwt.isEditor], settingsController.updateSettingById)
-router.delete('/:settingId', [authJwt.verifyToken, authJwt.isEditor], settingsController.deleteSettingById)
 
 export default router
-
-// Dejamos abierta la posibilidad de incorporar más global settings pero no las implementamos
