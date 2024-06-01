f39fbf0ccf4efd7d67e8520b50fdc0b235a586cb
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-06-01 16:49:43 +0200

    keep original file name when uploading through multer

--- backend/src/routes/upload.routes.js
+++ backend/src/routes/upload.routes.js
@@ -3,21 +3,41 @@ import multer from 'multer'
 
 const router = Router()
 
-const uploadImg = multer({
-    dest: 'public/uploads/images',
-//destination folder is automatically created if it's not available
-    limits: {
-        fileSize: 5000000 // 5mb
-    },
-    fileFilter: (req, file, callback) => {
-        console.log(file);
-        if (!file.originalname.match(/\.(png|jpeg|jpg)$/)) {
-            return callback(new Error('Please upload a Picture(PNG or JPEG)'))
-        }
-        callback(undefined, true);
-    }
+const storageConfig = multer.diskStorage({
+    // destinations is uploads folder 
+    // under the project directory
+  destination: "public/uploads/images",
+  filename: (req, file, res) => {
+        // file name is prepended with current time
+        // in milliseconds to handle duplicate file names
+      res(null, Date.now() + "-" + file.originalname);
+  },
+});
 
-})
+// file filter for filtering only images
+const fileFilterConfig = function(req, file, cb) {
+    if (file.mimetype === "image/jpeg"
+        || file.mimetype === "image/png") {
+          // calling callback with true
+          // as mimetype of file is image
+        cb(null, true);
+    } else {
+          // false to indicate not to store the file
+        cb(null, false);
+    }
+};
+
+// creating multer object for storing
+// with configuration
+const uploadImg = multer({
+    // applying storage and file filter
+  storage: storageConfig,
+  limits: {
+        // limits file size to 5 MB
+      fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilterConfig,
+});
 
 router.post('/image', uploadImg.single('image'), (req, res) => {
     res.json({data: "success"})
