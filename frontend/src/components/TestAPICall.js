2277c0999c00985f9dd54b6710ae6934964ddd77
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-06-01 15:15:59 +0200

    volume paths

--- frontend/src/components/TestAPICall.js
+++ frontend/src/components/TestAPICall.js
@@ -33,7 +33,8 @@ function TestAPICall() {
             url={modelList[0]?.modelURL || "https://res.cloudinary.com/dahr27egc/image/upload/v1709658481/model_psdrba.glb"}
           />
       </Canvas>
-      <img src="/logo192.png" alt="Example image for testing purposes"/>
+      <img src="/ImagePreset01.png" alt="Example image 1 for testing purposes"/>
+      <img src="/images/ImagePreset01.png" alt="Example image 2 for testing purposes"/>
       {/* <Canvas>
           <directionalLight position={[1,2,3]} intensity={4.5}/>
           <ambientLight intensity={1.5} />
