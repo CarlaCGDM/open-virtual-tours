b0b2c798624bb9ae8083a9a1f0916608e0139e64
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2025-02-18 00:26:11 +0100

    bug fixing

--- frontend/src/components/canvases/ModelParser.js
+++ frontend/src/components/canvases/ModelParser.js
@@ -1,6 +1,6 @@
 // Component that reads the data of a GLTF/GLB file and returns the JSON file with the data to the parent component
 
-import { React,useEffect } from 'react'
+import { React, useEffect } from 'react'
 import { Canvas } from "@react-three/fiber"
 import { Clone, useGLTF } from '@react-three/drei'
 import extractMarkerData from '../../utils/modelUtils.js'
@@ -10,20 +10,19 @@ const ModelParser = (props) => {
     // Get the 3D model
     const model = useGLTF(props.modelURL ? `${process.env.REACT_APP_UPLOADS_ROOT + props.modelURL}` : `${process.env.REACT_APP_UPLOADS_ROOT}/uploads/environments/DemoMuseum01.glb`)
 
-    if (model) {
+    useEffect(() => {
+        if (model) {
+            // Get the markers data
+            const markerData = extractMarkerData(model)
 
-          // Get the markers data
-        const markerData = extractMarkerData(model)
-
-        // Send data to parent component
-        useEffect(() => { props.updateMarkerData(markerData) }, [model]);
-
-    }
-  
+            // Send data to parent component
+            props.updateMarkerData(markerData)
+        }
+    }, [model]);
 
     return (
         <Canvas>
-            <Clone object={model.scene}/>
+            <Clone object={model.scene} />
         </Canvas>
     )
 }
